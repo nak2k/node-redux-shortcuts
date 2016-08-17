a10eddd8f50f071527619973cd1c675f47f32d8e
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2016-08-17 11:44:56 +0900

    Export Mousetrap #14

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import { bind } from 'mousetrap';
+import Mousetrap from 'mousetrap';
+
+export { Mousetrap };
+export const mousetrap = Mousetrap();
 
 export const bindShortcut = (keys, actionCreator, preventDefault) => dispatch =>
   (typeof actionCreator === 'function'
-    ? bind(keys, e => (dispatch(actionCreator()), !preventDefault))
-    : bind(keys, e => (actionCreator.forEach(actionCreator => dispatch(actionCreator())), !preventDefault)));
+    ? mousetrap.bind(keys, e => (dispatch(actionCreator()), !preventDefault))
+    : mousetrap.bind(keys, e => (actionCreator.forEach(actionCreator => dispatch(actionCreator())), !preventDefault)));
 
 export const bindShortcuts = (...shortcuts) => dispatch => shortcuts.forEach(shortcut => bindShortcut(...shortcut)(dispatch));
